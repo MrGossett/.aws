6e8208d12b4276ac583d437470bd986e13da0a6e
Tim Gossett <MrGossett@users.noreply.github.com>
2020-07-27 15:24:57 -0400

    Update organizations.tf

--- tf/organizations.tf
+++ tf/organizations.tf
@@ -1,25 +1,3 @@
 resource "aws_organizations_organization" "org" {
   feature_set = "ALL"
 }
-
-resource "aws_organizations_policy" "ai_opt_out" {
-  name = "AIServicesOptOutAll"
-  content = jsonencode({
-    services = {
-      "@@operators_allowed_for_child_policies" = ["@@none"],
-      default = {
-        "@@operators_allowed_for_child_policies" = ["@@none"],
-        opt_out_policy = {
-          "@@operators_allowed_for_child_policies" = ["@@none"],
-          "@@assign"                               = "optOut"
-        }
-      }
-    }
-  })
-}
-
-resource "aws_organizations_policy_attachment" "ai_opt_out_root" {
-  for_each  = toset(aws_organizations_organization.org.roots.*.id)
-  policy_id = aws_organizations_policy.ai_opt_out.id
-  target_id = each.value
-}
